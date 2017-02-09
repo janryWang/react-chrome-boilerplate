710bef6711a3285342714c26901e83e9b562da58
zhili.wzl <zhili.wzl@alibaba-inc.com>
2017-02-09 12:05:09 +0800

    refactor(message): delete other code

--- src/common/message.js
+++ src/common/message.js
@@ -1,46 +1,9 @@
 import PubSub from 'pubsub-js'
 
-if (chrome.extension) {
-    chrome.extension.onMessage.addListener(
-        (request) => {
-            PubSub.publish('CONTENT_DISPATCH', request)
-        }
-    )
-}
-
-PubSub.subscribe('CONTENT_DISPATCH', (msg, action) => {
-
-})
-
-
-PubSub.subscribe('BACKGROUND_DISPATCH', (msg, action) => {
-    if (chrome) {
-        if (chrome.tabs) {//background
-            chrome.tabs.query({
-                active: true,
-                currentWindow: true
-            }, (tabs) => {
-                chrome.tabs.sendMessage(tabs[0].id, action)
-            })
-        } else if (chrome.extension) {//content
-            chrome.extension.sendMessage(action)
-        }
-    }
-})
-
-
-const getEnv = () => {
-    if (chrome.tabs) {
-        return 'background'
-    } else {
-        return 'content'
-    }
-}
 
 
 const dispatch = (event, payload) => {
-    const env = getEnv()
-    if (env == 'background') {
+    if (chrome.tabs) {
         chrome.tabs.query({
             active: true,
             currentWindow: true
